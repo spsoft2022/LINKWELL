13c48652c78cf32ceb757adfa05d6258e12eedfd
vijayakumar.kella <vijayakumar.kella@spsoftglobal.com>
2026-01-20 10:52:25 +0530

    Floor page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LinkwellProductionSystem.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("floor-preview")]
+        public IActionResult FloorPreview(int station, int model)
+        {
+            ViewBag.StationId = station;
+            ViewBag.ModelId = model;
+            return View();
+        }
+
+    }
+}
